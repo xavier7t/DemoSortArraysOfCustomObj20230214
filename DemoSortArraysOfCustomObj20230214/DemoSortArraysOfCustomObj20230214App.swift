78e91d08706916afd27bebc80475cb01ec11fcad
Xavier <32859092+xavier7t@users.noreply.github.com>
2023-02-14 20:54:37 -0500

    Initial Commit

---
+++ DemoSortArraysOfCustomObj20230214/DemoSortArraysOfCustomObj20230214App.swift
@@ -0,0 +1,17 @@
+//
+//  DemoSortArraysOfCustomObj20230214App.swift
+//  DemoSortArraysOfCustomObj20230214
+//
+//  Created by Xavier on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoSortArraysOfCustomObj20230214App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
